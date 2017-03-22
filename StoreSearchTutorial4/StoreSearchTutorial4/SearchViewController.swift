821837fbca5124c07de0352b3ff2c6937480d7e0
Paweł Liczmański <paweliczek@gmail.com>
2017-03-22 20:38:50 +0100

    beggining

---
+++ StoreSearchTutorial4/StoreSearchTutorial4/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreSearchTutorial4
+//
+//  Created by Paweł Liczmański on 22.03.2017.
+//  Copyright © 2017 Paweł Liczmański. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
