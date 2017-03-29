ed029133dca3da0da559b61dcded1703bda68c4d
Paweł Liczmański <paweliczek@gmail.com>
2017-03-29 20:20:24 +0200

    konfiguracja LandscapeVC stworzenie przycisków i określenie ich liczby

--- StoreSearchTutorial4/StoreSearchTutorial4/LandscapeViewController.swift
+++ StoreSearchTutorial4/StoreSearchTutorial4/LandscapeViewController.swift
@@ -48,11 +48,91 @@ class LandscapeViewController: UIViewController {
                                    y: view.frame.size.height - pageControl.frame.size.height,
                                    width: view.frame.size.width,
                                    height: pageControl.frame.size.height)
+        
+        if firstTime {
+            firstTime = false
+            tileButtons(searchResults)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+//MARK: - METHODS
+    
+    private func tileButtons(_ searchResults: [SearchResult]) {
+        var columnsPerPage = 5
+        var rowsPerPage = 3
+        var itemWidth: CGFloat = 96
+        var itemHeight: CGFloat = 88
+        var marginX: CGFloat = 0
+        var marginY: CGFloat = 20
+        
+        let scrollViewWidth = scrollView.bounds.size.width
+        
+        switch scrollViewWidth {
+        case 568:
+            columnsPerPage = 6
+            itemWidth = 94
+            marginX = 2
+            
+        case 667:
+            columnsPerPage = 7
+            itemWidth = 95
+            itemHeight = 98
+            marginX = 1
+            marginY = 29
+            
+        case 736:
+            columnsPerPage = 8
+            rowsPerPage = 4
+            itemWidth = 92
+
+        default:
+            break
+        }
+        
+        let buttonWidth: CGFloat = 82
+        let buttonHeight: CGFloat = 82
+        let paddingHorz = (itemWidth - buttonWidth) / 2
+        let paddinfVert = (itemHeight - buttonHeight) / 2
+        
+        var row = 0
+        var column = 0
+        var x = marginX
+        
+        for (index, searchResult) in searchResults.enumerated() {
+            
+            let button = UIButton(type: .system)
+            button.backgroundColor = UIColor.white
+            button.setTitle("\(index)", for: .normal)
+            
+            button.frame = CGRect(x: x + paddingHorz,
+                                  y: marginY + CGFloat(row)*itemHeight + paddinfVert,
+                                  width: buttonWidth,
+                                  height: buttonHeight)
+            scrollView.addSubview(button)
+            // buttony ida w kolumnach od gory do dołu, aż do momentu row == rowsPerPage wtedy dodaje sie += 1 column
+            row += 1
+            if row == rowsPerPage {
+                row = 0; x += itemWidth; column += 1
+                // jak dojdziemy do ostatniej kolumny to dodajemy margines ostatni na jej stronie i pierwszy na kolejnej dlatego * 2
+                if column == columnsPerPage {
+                    column = 0; x += marginX * 2
+                }
+            }
+        }
+        
+        let buttonsPerPage = columnsPerPage * rowsPerPage
+        // dlatego +1 bo jak searchResults.count bedzie mniejszy niz buttons per Page to daloby wynik 0 stron
+        let numPages = 1 + (searchResults.count - 1) / buttonsPerPage
+        
+        scrollView.contentSize = CGSize(width: CGFloat(numPages)*scrollViewWidth,
+                                        height: scrollView.bounds.size.height)
+        print("Number of pages: \(numPages)")
+        
     }
     
 
